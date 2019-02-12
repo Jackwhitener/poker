392057107160a621308a65fefc5ef446aebd7e17
Jackwhitener <jackwhitener1@gmail.com>
2019-02-12 09:23:07 -0500

    Third test now passes.

--- poker.rb
+++ poker.rb
@@ -1,3 +1,11 @@
 def randomcard(amount)
-    return 1
+    cards = (1..52).to_a
+    drawncards = Array.new
+    amount.times do
+        card = cards.sample
+        cards.delete_at(card)
+        drawncards << card
+    end
+puts drawncards
+return drawncards
 end
--- poker_test.rb
+++ poker_test.rb
@@ -5,12 +5,12 @@ class Test < Minitest::Test
         assert_equal(1,1)
     end
     def test_card_number
-        possiblenumbers = (1..52)
+        possiblenumbers = (1..52).to_a
         cardnum = randomcard(1)
-        assert_equal(true, possiblenumbers.include?(cardnum))
+        assert_equal(true, possiblenumbers.include?(cardnum[0]))
     end
     def test_multiple_cards
-        possiblenumbers = (1..52)
+        possiblenumbers = (1..52).to_a
         cardnum = randomcard(2)
         assert_equal(true, possiblenumbers.include?(cardnum[0]))
         assert_equal(true, possiblenumbers.include?(cardnum[1]))
