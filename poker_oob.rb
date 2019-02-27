7ec05ad860028989d3e1be98f41a807b7fd418af
Jackwhitener <jackwhitener1@gmail.com>
2019-02-27 09:21:31 -0500

    fixed errors, one test fails

--- poker_oob.rb
+++ poker_oob.rb
@@ -7,16 +7,17 @@ class Hand
             @cards_in_hand = []
         end
     end
+    def draw(deckdrawnfrom)
+
+    end
 end
-class Deck
+class Deck 
     attr_accessor :cards_in_deck
-    def initialize(cards_in_deck)
+    def initialize
+        cards_in_deck = (1..52).to_a
         @cards_in_deck = cards_in_deck
     end
     def shufflecards
        return cards_in_deck.shuffle
     end
-end
-def draw(number, deck)
-
 end
--- poker_oob_test.rb
+++ poker_oob_test.rb
@@ -13,17 +13,22 @@ class Test < Minitest::Test
         assert_equal([1,5,7,9,13], hand.cards_in_hand)
     end
     def test_for_deck
-        deck = Deck.new((1..52).to_a)
+        deck = Deck.new
         assert_equal(Deck, deck.class)
     end
+    def test_for_deck_length
+        deck = Deck.new
+        assert_equal(52, deck.cards_in_deck.length)
+    end
     def test_for_deck_shuffle
-        deck = Deck.new((1..52).to_a)
+        deck = Deck.new
         deck.cards_in_deck = deck.shufflecards
         assert_equal(false, (1..52).to_a == deck.cards_in_deck)
     end
     def test_for_draw
-        deck = Deck.new((1..52).to_a)
-        hand = Hand.new(draw(5, deck))
-        assert_equal(5, hand.length)
+        deck = Deck.new
+        hand = Hand.new(nil)
+        hand.draw(deck.cards_in_deck)
+        assert_equal(5, hand.cards_in_hand.length)
     end
 end
