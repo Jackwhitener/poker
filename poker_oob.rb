b474899030c07a193527bcf1c2950c155038e0a9
Jackwhitener <jackwhitener1@gmail.com>
2019-02-27 15:46:30 -0500

    Queen high test passes

--- poker_oob.rb
+++ poker_oob.rb
@@ -27,5 +27,7 @@ end
 def handjudger(hand)
     return "Ace High" if hand.cards_in_hand.include?(52) || hand.cards_in_hand.include?(51) || hand.cards_in_hand.include?(50) || hand.cards_in_hand.include?(49)
     return "King High" if hand.cards_in_hand.include?(48) || hand.cards_in_hand.include?(47) || hand.cards_in_hand.include?(46) || hand.cards_in_hand.include?(45)
+    return "Queen High" if hand.cards_in_hand.include?(44) || hand.cards_in_hand.include?(43) || hand.cards_in_hand.include?(42) || hand.cards_in_hand.include?(41)
+
 
 end
