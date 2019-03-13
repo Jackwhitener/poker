76896c7df629d5d664be667ee589c2872d005656
Jackwhitener <jackwhitener1@gmail.com>
2019-03-13 15:52:49 -0400

    hand displays are all purty

--- console_app.rb
+++ console_app.rb
@@ -7,5 +7,5 @@ redhand.draw(deck, 5)
 blackhand.draw(deck, 5)
 redhandlated = handslator(redhand.cards_in_hand)
 blackhandlated = handslator(blackhand.cards_in_hand)
-puts "Black hand: #{blackhand.cards_in_hand}, #{blackhandlated}, #{handjudger(blackhand)}"
-puts "Red hand: #{redhand.cards_in_hand}, #{redhandlated} #{handjudger(redhand)}"
+puts "Black hand: #{blackhandlated[0]}, #{blackhandlated[1]}, #{blackhandlated[2]}, #{blackhandlated[3]}, #{blackhandlated[4]}, #{handjudger(blackhand)}"
+puts "Red hand: #{redhandlated[0]}, #{redhandlated[1]}, #{redhandlated[2]}, #{redhandlated[3]}, #{redhandlated[4]}, #{handjudger(redhand)}"
